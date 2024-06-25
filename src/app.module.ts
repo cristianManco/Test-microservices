03a0ac932e7704bc78d137e95664782014f7be88
cristianManco <camilomanco2005@gmail.com>
2024-06-25 14:13:22 -0500

    folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { TipsModule } from './tips/tips.module';
 import { AuthModule } from './auth/auth.module';
 import { LogsModule } from './logs/logs.module';
+import { LogsService } from './logs/service/logs.service';
+import { LogsController } from './logs/entities/controller/logs.controller';
+import { LogsModule } from './logs/logs.module';
 
 
 @Module({
   imports: [TipsModule, AuthModule, LogsModule],
-  controllers: [],
-  providers: [],
+  controllers: [LogsController],
+  providers: [LogsService],
 })
 export class AppModule {}

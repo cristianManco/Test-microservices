e1908ccd34e00872404ba918140fdb3ff87a53e7
cristianManco <camilomanco2005@gmail.com>
2024-06-27 15:44:06 -0500

    general modificaciones

--- src/auth/entities/api-key.entity.ts
+++ src/auth/entities/api-key.entity.ts
@@ -4,14 +4,38 @@ import { Document } from 'mongoose';
 export type ApiKeyDocument = ApiKey & Document;
 @Schema()
 export class ApiKey {
+  @Prop({ unique: true })
+  id: string;
+
+  @Prop({ required: true })
+  system_name: string;
+
   @Prop({ required: true, unique: true })
   key: string;
+
+  @Prop()
+  expiration: Date;
 
   @Prop({ default: true })
   isActive: boolean;
 
-  @Prop({ default: Date.now })
-  createdAt: Date;
+  @Prop({ default: null })
+  createdAt: Date | null;
+
+  @Prop()
+  createBy: string;
+
+  @Prop({ default: null })
+  updatedAt: Date | null;
+
+  @Prop()
+  updateBy: string;
+
+  @Prop({ default: null })
+  deletedAt: Date | null;
+
+  @Prop()
+  deleteBy: string;
 }
 
 export const ApiKeySchema = SchemaFactory.createForClass(ApiKey);
